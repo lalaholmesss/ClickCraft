a44c0c366cefae765df233f26e5a322e60d2b43e
lalaholmesss <lalaholmesart00@gmail.com>
2025-03-04 00:05:04 +0400

    Add component to Portfolios page

---
+++ src/components/PotfoliosPage/AllPortfolios/AllPortfolios.jsx
@@ -0,0 +1,119 @@
+import styles from "./AllPortfolios.module.css";
+
+export default function AllPortfolios() {
+    return(
+        <div className={styles.container}>
+            <h2>All Portfolios</h2>
+            <div className={styles.portfolioContainer}>
+                <div className={styles.portfolioCard}>
+                    <img src="images/Portfolio-card.png" alt="" />
+                    <div className={styles.icons}>
+                        <button>Customize This</button>
+                        <div className={styles.favIcons}>
+                            <img src="images/Favorite.png" alt="" />
+                            <p>99</p>
+                            <img src="images/Eye.png" alt="" />
+                            <p>999</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.portfolioCard}>
+                    <img src="images/Portfolio-card.png" alt="" />
+                    <div className={styles.icons}>
+                        <button>Customize This</button>
+                        <div className={styles.favIcons}>
+                            <img src="images/Favorite.png" alt="" />
+                            <p>99</p>
+                            <img src="images/Eye.png" alt="" />
+                            <p>999</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.portfolioCard}>
+                    <img src="images/Portfolio-card.png" alt="" />
+                    <div className={styles.icons}>
+                        <button>Customize This</button>
+                        <div className={styles.favIcons}>
+                            <img src="images/Favorite.png" alt="" />
+                            <p>99</p>
+                            <img src="images/Eye.png" alt="" />
+                            <p>999</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.portfolioCard}>
+                    <img src="images/Portfolio-card.png" alt="" />
+                    <div className={styles.icons}>
+                        <button>Customize This</button>
+                        <div className={styles.favIcons}>
+                            <img src="images/Favorite.png" alt="" />
+                            <p>99</p>
+                            <img src="images/Eye.png" alt="" />
+                            <p>999</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.portfolioCard}>
+                    <img src="images/Portfolio-card.png" alt="" />
+                    <div className={styles.icons}>
+                        <button>Customize This</button>
+                        <div className={styles.favIcons}>
+                            <img src="images/Favorite.png" alt="" />
+                            <p>99</p>
+                            <img src="images/Eye.png" alt="" />
+                            <p>999</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.portfolioCard}>
+                    <img src="images/Portfolio-card.png" alt="" />
+                    <div className={styles.icons}>
+                        <button>Customize This</button>
+                        <div className={styles.favIcons}>
+                            <img src="images/Favorite.png" alt="" />
+                            <p>99</p>
+                            <img src="images/Eye.png" alt="" />
+                            <p>999</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.portfolioCard}>
+                    <img src="images/Portfolio-card.png" alt="" />
+                    <div className={styles.icons}>
+                        <button>Customize This</button>
+                        <div className={styles.favIcons}>
+                            <img src="images/Favorite.png" alt="" />
+                            <p>99</p>
+                            <img src="images/Eye.png" alt="" />
+                            <p>999</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.portfolioCard}>
+                    <img src="images/Portfolio-card.png" alt="" />
+                    <div className={styles.icons}>
+                        <button>Customize This</button>
+                        <div className={styles.favIcons}>
+                            <img src="images/Favorite.png" alt="" />
+                            <p>99</p>
+                            <img src="images/Eye.png" alt="" />
+                            <p>999</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.portfolioCard}>
+                    <img src="images/Portfolio-card.png" alt="" />
+                    <div className={styles.icons}>
+                        <button>Customize This</button>
+                        <div className={styles.favIcons}>
+                            <img src="images/Favorite.png" alt="" />
+                            <p>99</p>
+                            <img src="images/Eye.png" alt="" />
+                            <p>999</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Portfolios.jsx
+++ src/pages/Portfolios.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AllPortfolios from '../components/PotfoliosPage/AllPortfolios/AllPortfolios';
 
 export default function Portfolios () {
   return (
     <div>
-      Portfolios
+      <AllPortfolios />
     </div>
   )
 }
